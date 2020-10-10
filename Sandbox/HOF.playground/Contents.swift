3f0eb48386b0bb3cb1c2619c1dcc7d9edd589c74
dsseo <sorld0603@snu.ac.kr>
2020-10-10 11:15:30 +0900

    update

--- Sandbox/HOF.playground/Contents.swift
+++ Sandbox/HOF.playground/Contents.swift
@@ -42,3 +42,29 @@ for char in characters {
     }
 }
 print(count)
+
+
+let array1 = [1, 2, 2, 1]
+let array2 = [1, 2, 3, 4, 3, 2, 1]
+let array3 = [1, 3, 4, 4, 1]
+let array4 = [1, 2, 3, 1]
+
+// 배열의 순서를 반대로 바꿔도 같은 배열인지를 판별
+func isPalindromeArray(arr: [Int])->Bool{
+    let size = arr.count
+    var isPalindrome = true
+    
+    for i in 1...size/2{
+        if(arr[i] != arr[size - i - 1]){
+            isPalindrome = false
+        }
+    }
+    
+    return isPalindrome
+}
+
+print(isPalindromeArray(arr: array4))
+
+
+
+
